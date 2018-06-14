0920d9dc3a956a74c366802e83108098c8414903
PascalP <chat@lespascals.org>
2018-06-14 19:24:29 +0200

    Fix a missing dependency in navigation-menu

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 import { LogoutComponent } from './logout/logout.component'
+import { NavigationMenuComponent } from './navigation-menu/navigation-menu.component'
 import { ProfileComponent } from './profile/profile.component'
 import { UserRoutingModule } from './user-routing.module'
 
 @NgModule({
   imports: [CommonModule, UserRoutingModule],
-  declarations: [ProfileComponent, LogoutComponent],
+  declarations: [ProfileComponent, LogoutComponent, NavigationMenuComponent],
 })
 export class UserModule {}
