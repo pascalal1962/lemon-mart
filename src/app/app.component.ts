891ff8eeb2e5349faea5a2316c3cfbc6ed35aae2
PascalP <chat@lespascals.org>
2018-06-15 17:15:42 +0200

    Implenting common validators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,19 @@
 import { Component } from '@angular/core'
 import { MatIconRegistry } from '@angular/material/icon'
 import { DomSanitizer } from '@angular/platform-browser'
+import { AuthService } from './auth/auth.service'
 
 @Component({
   selector: 'app-root',
   template: `
     <mat-toolbar color="primary" fxLayoutGap="8px">
-      <button mat-icon-button><mat-icon>menu</mat-icon></button>
+      <button *ngIf="displayAccountIcons" mat-icon-button><mat-icon>menu</mat-icon></button>
       <a mat-button routerLink="/home"><mat-icon svgIcon="lemon"></mat-icon><span class="mat-h2">LemonMart</span></a>
       <span class="flex-spacer"></span>
-      <button mat-mini-fab routerLink="/user/profile" matTooltip="Profile" aria-label="User Profile">
-        <mat-icon>account_circle</mat-icon></button>
-      <button mat-mini-fab routerLink="/user/logout" matTooltip="Logout" aria-label="Logout">
-        <mat-icon>lock_open</mat-icon></button>
+      <button *ngIf="displayAccountIcons" mat-mini-fab routerLink="/user/profile"
+        matTooltip="Profile" aria-label="User Profile"><mat-icon>account_circle</mat-icon></button>
+      <button *ngIf="displayAccountIcons" mat-mini-fab routerLink="/user/logout"
+        matTooltip="Logout" aria-label="Logout"><mat-icon>lock_open</mat-icon></button>
     </mat-toolbar>
     <router-outlet></router-outlet>
   `,
@@ -20,8 +21,16 @@ import { DomSanitizer } from '@angular/platform-browser'
 })
 export class AppComponent {
   title = 'app'
+  displayAccountIcons = false
 
-  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+  constructor(
+    iconRegistry: MatIconRegistry,
+    sanitizer: DomSanitizer,
+    private authService: AuthService
+  ) {
+    this.authService.authStatus.subscribe(
+      authStatus => (this.displayAccountIcons = authStatus.isAuthenticated)
+    )
     iconRegistry.addSvgIcon(
       'lemon',
       sanitizer.bypassSecurityTrustResourceUrl('assets/img/icons/lemon.svg')
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core'
+import { AuthService } from '../auth/auth.service'
 
 @Component({
   selector: 'app-home',
@@ -19,8 +20,17 @@ import { Component, OnInit } from '@angular/core'
   ],
 })
 export class HomeComponent implements OnInit {
-  displayLogin = true
-  constructor() {}
+  private _displayLogin = true
 
-  ngOnInit() {}
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    this.authService.authStatus.subscribe(
+      authStatus => (this._displayLogin = !authStatus.isAuthenticated)
+    )
+  }
+
+  get displayLogin() {
+    return this._displayLogin
+  }
 }
